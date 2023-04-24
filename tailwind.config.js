f49079ca9437123c4777bd2b2c9d15fe5072dda0
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-24 04:40:56 +0300

    Set up tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,39 @@
 /* eslint-env node */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    colors: {
+      // Primary
 
+      'bright-blue': 'hsl(220, 98%, 61%)',
+
+      // Light theme
+
+      'very-light-gray': 'hsl(0, 0%, 98%)',
+      'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+      'light-grayish-blue': 'hsl(233, 11%, 84%)',
+      'dark-grayish-blue': 'hsl(236, 9%, 61%)',
+      'very-dark-grayish-blue': 'hsl(235, 19%, 35%)',
+
+      // Dark theme
+
+      'dt-very-dark-blue': 'hsl(235, 21%, 11%)',
+      'dt-very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+      'dt-light-grayish-blue': 'hsl(234, 39%, 85%)',
+      'dt-light-grayish-blue-hover': 'hsl(236, 33%, 92%)',
+      'dt-dark-grayish-blue': 'hsl(234, 11%, 52%)',
+      'dt-very-dark-grayish-blue': 'hsl(233, 14%, 35%)',
+      'dt-very-dark-grayish-blue-hover': 'hsl(237, 14%, 26%)'
+    },
+    gradientColorStops: {
+      'check-bg-from': 'hsl(192, 100%, 67%)',
+      'check-bg-to': 'hsl(280, 87%, 65%)'
+    },
+    fontFamily: {
+      'josefin-sans': ['Josefin Sans', 'sans-serif']
+    },
+    extend: {}
+  },
+  plugins: []
+}

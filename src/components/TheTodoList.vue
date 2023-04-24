d033667e29bb161589840390a01765d06a7c1a63
Dana Yatsuta <danayatsuta@gmail.com>
2023-04-24 07:48:00 +0300

    Fix incorrect gap in todo list

--- src/components/TheTodoList.vue
+++ src/components/TheTodoList.vue
@@ -12,7 +12,7 @@ export default {
 
 <template>
   <div class="-translate-y-24 px-6 md:-translate-y-36">
-    <div class="mx-auto flex max-w-xl flex-col gap-6">
+    <div class="mx-auto flex max-w-xl flex-col gap-4 md:gap-6">
       <TodoListForm />
 
       <AppCard class="divide-y divide-very-light-grayish-blue shadow-xl">

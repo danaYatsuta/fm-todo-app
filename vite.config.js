ffab71927a7622658a929f9ba43081e8039e4cf6
Dana Yatsuta <danayatsuta@gmail.com>
2024-04-09 18:45:03 +0300

    Update base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-mentor-todo-app/',
+  base: '/fm-todo-app/',
   plugins: [vue()],
   resolve: {
     alias: {
